df4692578e7ac027e7171573af21fba819da554f
neelakantan.krishnaswami <neelakantan.krishnaswami@8d7f99d0-916d-11dd-b9ac-2b9acee0b3cf>
2010-08-28 21:33:01 +0000

    added new syntactic combinators

--- trunk/popl11/adjointcode/combinators.mli
+++ trunk/popl11/adjointcode/combinators.mli
@@ -1,6 +1,7 @@
 (* Term formers for changing DSL syntax into ML syntax, with one for each combinator *)
 
-type ast = string -> string -> string -> Camlp4.PreCast.Ast.loc -> Camlp4.PreCast.Ast.expr
+type ast = string -> string -> string ->
+    Camlp4.PreCast.Ast.loc -> Camlp4.PreCast.Ast.expr
 
 val id0 : string -> ast
 val id1 : string -> ast -> ast
@@ -8,6 +9,7 @@ val id2 : string -> ast -> ast -> ast
 val id0' : string -> ast
 val id1' : string -> ast -> ast
 val id2' : string -> ast -> ast -> ast
+
 val id : ast
 val compose : ast -> ast -> ast
 val one : ast
@@ -16,10 +18,27 @@ val pi1 : ast
 val pi2 : ast
 val curry : ast -> ast
 val eval : ast
-
-val guireturn : ast
-val bind : ast -> ast
-val strength : ast
+val fix : ast
+val cons : ast
+val head : ast
+val tails : ast 
+val stream : ast -> ast
+val sweak : ast
+val spair : ast
+val scurry : ast
+val seval : ast
+val swap : ast
+val scomposel : ast -> ast
+val scomposer : ast -> ast
+val eval' : ast
+val tensor : ast -> ast -> ast
+val rho : ast   
+val rho' : ast   
+val lambda : ast
+val lambda' : ast
+val assocl : ast
+val assocr : ast 
+val sym : ast 
 
 val flip : ast -> ast
 val one' : ast
@@ -29,27 +48,18 @@ val prod' : ast
 val paird : ast
 val paird' : ast
 val apply : ast 
-val contract : ast
-val contract' : ast
+
 val varepsilon : ast
 val eta : ast
-val value : ast -> ast
-val omega : ast -> ast
+val f : ast -> ast
+val g : ast -> ast
+
 val discrete : Camlp4.PreCast.Ast.expr -> ast
 val unitize  : Camlp4.PreCast.Ast.expr -> Camlp4.PreCast.Ast.loc -> Camlp4.PreCast.Ast.expr
-val fix : ast
-val cons : ast
-val sweak : ast
-val spair : ast
-val scurry : ast
-val seval : ast
-val swap : ast
-val scomposel : ast -> ast
-val scomposer : ast -> ast
-val eval' : ast
+
 val times : ast -> ast -> ast
-val assocr : ast
-val assocl : ast
+val passocr : ast
+val passocl : ast
 val uncurry : ast
 
 (* Reassociate an environment *)
